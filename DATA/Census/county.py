c9358dc40e66dacbae4a168f366b42ab61452f5c
habrasek <65578198+habrasek@users.noreply.github.com>
2021-05-12 15:02:06 -0400

    a list of counties lime operates in

---
+++ DATA/Census/county.py
@@ -0,0 +1,82 @@
+counties = [
+#Arkansas
+'Pulaski County',
+
+#California
+'Placer County', 'Dorado', 'Los Angeles', 'Alameda County', 'Sacramento County', 'San Francisco County', 'Santa Clara County', 'Santa Barbara County',
+
+#Colorado
+'Denver County',
+
+#DC
+'District of Columbia',
+
+#Florida
+'Miami-Dade County', 'Hillsborough County', 'Orange County', 
+
+#GA
+'Bulloch County',
+
+#ID
+'Ada County',
+
+#IL
+'Cook County', 
+
+#IN
+'Monroe County', 'Marion County',
+
+#KY
+'Jefferson County', 'Fayette County',
+
+#MD
+'Baltimore County',
+
+#MA
+'Suffolk County',
+
+#MI
+'Wayne County', 'Ingham County',
+
+#MN
+'Ramsey County', 'Hennepin County', 'Olmsted County', 
+
+#MO
+'Boone County', 'St. Louis County',
+
+#NE
+'Douglas County',
+
+#NY
+'Kings County', 'New York County', 'Queens County',
+
+#NC
+'Mecklenburg County', 'Guilford County', 'Wake County', 'Durham County',
+
+#OH
+'Hamilton County', 'Cuyahoga County', 'Franklin County', 'Butler County', 'Lucas County',
+
+#OK
+'Oklahoma County', 'Tulsa County', 
+
+#OR
+'Multnomah County', 
+
+#TN
+'Davidson County',
+
+#TX
+'Travis County', 'Nueces County', 'Dallas County', 'Lubbock County',
+
+#UT
+'Weber County', 'Utah County', 
+
+#VA
+'Norfolk city', 'Roanoke City',
+
+#WA
+'King County', 'Pierce County', 'Spokane County',
+
+#WI
+'Milwaukee County'
+]
